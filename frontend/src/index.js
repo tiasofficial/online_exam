bb2297d95d7c3c331ad290a73b99f1ea7e1ab9e6
tiasofficial <tias.officialsiliguri@gmail.com>
2026-07-27 15:49:12 +0530

    update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
-import store from './redux/store';
+import { Provider } from 'react-redux';
+import store from './store';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(
+ReactDOM.render(
   <>
     <Provider store={store}>
-      <App />
+      <App/>
     </Provider>
-  </>
+  </>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
