3a596dfef7ba88f81e30a1777d61f607af17383e
tiasofficial <tias.officialsiliguri@gmail.com>
2026-07-27 17:09:18 +0530

    Add files via upload

---
+++ backend/services/answersheet.js
@@ -0,0 +1,40 @@
+var mongoose = require('mongoose')
+
+var answersheetSchema = new mongoose.Schema({
+  test : {
+    type : mongoose.Schema.Types.ObjectId,
+    ref : 'test',
+    required : true
+  },
+  student : {
+    type : mongoose.Schema.Types.ObjectId,
+    ref : 'user',
+    required : true
+  },
+  score : {
+    type : Number,
+    default : 0,
+    required : true
+  },
+  answers : [{
+    type : mongoose.Schema.Types.Mixed
+  }],
+  timeSpent : [{
+    type : Number
+  }],
+  revisitCounts : [{
+    type : Number
+  }],
+  startTime : {
+    type : Date
+  },
+  completed : {
+    type : Boolean,
+    required : true,
+    default : false
+  }
+},{
+  timestamps : true
+})
+
+module.exports = answersheetSchema;
